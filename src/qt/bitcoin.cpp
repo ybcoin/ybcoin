3fcb91783c6bb88a4dd5f8c073f57fb9389a58ab
ybcoin <ycoin@gmail.com>
2014-02-19 13:43:58 +0800

    Fix:initial splash window failed

--- src/qt/bitcoin.cpp
+++ src/qt/bitcoin.cpp
@@ -196,7 +196,9 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-    QSplashScreen splash(QPixmap(":/images/splash"), 0);
+    QImage img(":/images/splash");
+    QPixmap amap = QPixmap::fromImage(img);
+    QSplashScreen splash(amap);
     if (GetBoolArg("-splash", true) && !GetBoolArg("-min"))
     {
         splash.show();
@@ -205,7 +207,6 @@ int main(int argc, char *argv[])
     }
 
     app.processEvents();
-
     app.setQuitOnLastWindowClosed(false);
 
     try
