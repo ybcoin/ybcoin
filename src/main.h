34f76c00a5fddd7b247f0951f6a08716e8520610
dx11 <dx11coin@outlook.com>
2014-05-04 01:47:39 +0800

    Change Images and disable POS Changeover.

--- src/main.h
+++ src/main.h
@@ -25,7 +25,7 @@ class CInv;
 class CRequestTracker;
 class CNode;
 
-static const int nConsecutiveStakeSwitchHeight = 2;
+static const int nConsecutiveStakeSwitchHeight = 50000000;// We want POS to be continuous after POS only implementation
 static const unsigned int MAX_BLOCK_SIZE = 1000000;
 static const unsigned int MAX_BLOCK_SIZE_GEN = MAX_BLOCK_SIZE/2;
 static const unsigned int MAX_BLOCK_SIGOPS = MAX_BLOCK_SIZE/50;
